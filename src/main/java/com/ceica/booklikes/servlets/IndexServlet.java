a3b1cedc82f122fb4eaa037d2717aca4cbe817b5
SandTopo <guillermito356789@gmail.com>
2024-03-06 18:18:18 +0100

    LoginServlet ya funciona

--- src/main/java/com/ceica/booklikes/servlets/IndexServlet.java
+++ src/main/java/com/ceica/booklikes/servlets/IndexServlet.java
@@ -1,5 +1,6 @@
 package com.ceica.booklikes.servlets;
 
+import com.ceica.booklikes.Models.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,45 +12,17 @@ import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-@WebServlet(name = "indexServlet", value = "/index")
+@WebServlet(name = "indexServlet", value = "")
 public class IndexServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-       // int idbook= Integer.parseInt(request.getParameter("idbook"));
-        //User user= (User) request.getSession().getAttribute("user");
-        response.setHeader("Location", "/login");
-// Si el usuario no ha iniciado sesión, devuelve 303 See Other
-//        if (user == null) {
-//            response.setStatus(HttpServletResponse.SC_SEE_OTHER);
-//            response.setHeader("Location", "/login"); // Indica al cliente que debe ir a /login
-//
-//        }else {
-//
-//
-//            TaskController taskController = new TaskController();
-//            taskController.userLogged = user;
-//            taskController.deleteTask(idtask);
-//            List<Task> taskList = taskController.getAllTaskByUser();
-//            StringBuilder tbody = new StringBuilder();
-//            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-//
-//            for (Task task:taskList){
-//                tbody.append("<tr><td>").append(task.getIdtask()).append("</td>")
-//                        .append("<td>").append(task.getTitle()).append("</td>")
-//                        .append("<td>").append(task.getDescription()).append("</td>")
-//                        .append("<td>").append(sdf.format(task.getCreate_time())).append("</td>")
-//                        .append("<td>").append(sdf.format(task.getDeadline())).append("</td>")
-//                        .append("<td>").append("<i class=\"fa-solid fa-pen-to-square\"></i>")
-//                        .append("<i class=\"fa-solid fa-trash\" onClick=\"borrar(").append(task.getIdtask()).append(")\"></i>")
-//                        .append("</td></tr>")
-//                ;
-//
-//            };
-//            if(taskList.isEmpty()){
-//                tbody.append("<td>No hay tareas</td>");
-//            }
-//            PrintWriter out = response.getWriter();
-//            out.write(tbody.toString());
-//            //out.write("{\"msg\":\"task deleted\"}");
-//        }
+        User user = new User();
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+
+    }
+
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+
     }
 }
--- src/main/java/com/ceica/booklikes/servlets/LoginServlet.java
+++ src/main/java/com/ceica/booklikes/servlets/LoginServlet.java
@@ -1,6 +1,7 @@
 package com.ceica.booklikes.servlets;
 
 import com.ceica.booklikes.Models.User;
+import com.ceica.booklikes.controller.TaskController;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,35 +14,32 @@ import java.io.IOException;
 @WebServlet(name = "loginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
         User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
             request.getRequestDispatcher("login.jsp").forward(request, response);
         } else {
-                response.sendRedirect("user");
-            }
-        }
+            response.sendRedirect("user");
 
+        }
+    }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String user = request.getParameter("user");
+        String user = request.getParameter("email");
         String pass = request.getParameter("password");
-       // TaskController taskController = new TaskController();
+        TaskController taskController = new TaskController();
 
-//        if (taskController.login(user, pass)) {
-//            HttpSession session = request.getSession();
-//            session.setAttribute("user", taskController.userLogged);
-//            if (taskController.isAdmin()) {
-//                response.sendRedirect("admin");
-//            } else {
-//                response.sendRedirect("user");
-//            }
-//        } else {
-//            request.setAttribute("mensaje", "Usuario o Password incorrecto");
-//            request.getRequestDispatcher("login.jsp").forward(request, response);
-//
-//        }
+        if (taskController.login(user, pass)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", taskController.userLogged);
+            response.sendRedirect("user");
+
+        } else {
+            request.setAttribute("mensaje", "Usuario o Password incorrecto");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+
+        }
     }
-//
-//
 }
+//
+//
+
