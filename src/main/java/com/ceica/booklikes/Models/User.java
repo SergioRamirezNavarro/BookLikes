e79b30fffa9e9a79dd03db148dfebf80821a571a
SandTopo <guillermito356789@gmail.com>
2024-03-06 18:30:38 +0100

    User de prueba

---
+++ src/main/java/com/ceica/booklikes/Models/User.java
@@ -0,0 +1,92 @@
+package com.ceica.booklikes.Models;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class User extends ModeloBase {
+    private int idusuario;
+    private String nombre;
+    private String email;
+    private String contraseña;
+
+    public User() {
+    }
+
+    public User(int idusuario, String nombre, String email, String contraseña) {
+        this.idusuario = idusuario;
+        this.nombre = nombre;
+        this.email = email;
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    protected String getNombreTabla() {
+        return "usuario";
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "idusuario=" + idusuario +
+                ", nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", contraseña='" + contraseña + '\'' +
+                '}';
+    }
+
+    public User login(String email, String contraseña) {
+        User usuario = new User();
+        Connection conn = usuario.getConnection();
+        String sql = " select idusuario,nombre,email,contraseña from usuario " +
+                "where email=? and contraseña=?";
+        try {
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, email);
+            pst.setString(2, contraseña);
+            ResultSet resultSet = pst.executeQuery();
+            if (resultSet.next()) {
+                usuario.idusuario = resultSet.getInt("idusuario");
+                usuario.nombre = resultSet.getString("nombre");
+                usuario.email = resultSet.getString("email");
+                usuario.contraseña = resultSet.getString("contraseña");
+                return usuario;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
+
+
